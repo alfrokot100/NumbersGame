ac69cbae7374aaad53b40f88fbb8ea27e37fce3f
Affe O <alfrokot100@student.kau.se>
2025-10-17 12:26:37 +0200

    Lte fix i program

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace NumbersGame
 
         static void CheckGuess()
         {
-            Random random = new Random();//Calling out the Random generator to randomize numbers
+            Random random = new Random();//Calling out the Random generator to randomize the numbers
             int Number = random.Next(1, 21);
                           
             for (int i = 0; i < 5; i++)//For-loop and the input user get max 5 tries
@@ -29,9 +29,30 @@ namespace NumbersGame
         static void Main(string[] args)
         {
             Console.WriteLine("Gissa en siffra mellan 1 - 20");
-            CheckGuess();
+            GuessGame();
         }
 
+
+        static void GuessGame() 
+        {
+            Random random = new Random();
+            int number = random.Next(1, 21);
+
+            for(int i=0; i< 5; i++)
+            {
+                string input = Console.ReadLine();
+                bool guess = int.TryParse(input, out int result);
+
+                if (!guess) { Console.WriteLine("Felaktig input! Gör om och gör rätt"); i--; }
+
+                if (result > number) { Console.WriteLine("För stort nummer"); }
+                else if (result < number) { Console.WriteLine("För litet nummer"); }
+                else { Console.WriteLine("Du gissade rätt!"); return; }
+
+            }
+            Console.WriteLine($"Du gissa inte rätt nummer tyvärr. Siffran var {number}");
+
+        }
 
     }
 }
